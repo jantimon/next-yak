1602c69a8d6e9cc618da4f3cc72914a0fb47c9cd
Jan Nicklas <j.nicklas@me.com>
2023-10-18 14:57:56 +0200

    make use theme optional

---
+++ packages/next-yak/dist/context/baseContext.d.ts
@@ -0,0 +1,3 @@
+declare function getYakThemeContext(): {};
+
+export { getYakThemeContext };
--- packages/next-yak/runtime/styled.tsx
+++ packages/next-yak/runtime/styled.tsx
@@ -1,6 +1,5 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { CSSInterpolation, css } from "./cssLiteral.js";
-import React from "react";
 
 // the following export is not relative as "next-yak/context"
 // links to one file for react server components and
@@ -18,50 +17,78 @@ import type { YakTheme } from "./context/index.d.ts";
 
 type HtmlTags = keyof JSX.IntrinsicElements;
 
-function StyledFactory <THtmlTag extends HtmlTags>(Component: THtmlTag): <TProps extends Record<string, unknown>>(
+function StyledFactory<THtmlTag extends HtmlTags>(
+  Component: THtmlTag
+): <TProps extends Record<string, unknown>>(
   styles: TemplateStringsArray,
   ...values: CSSInterpolation<TProps & { theme: YakTheme }>[]
 ) => FunctionComponent<JSX.IntrinsicElements[THtmlTag] & TProps>;
-function StyledFactory (Component: string | FunctionComponent<any>) {
+function StyledFactory(Component: string | FunctionComponent<any>) {
   return <TProps extends Record<string, unknown>>(
     styles: TemplateStringsArray,
     ...values: CSSInterpolation<TProps>[]
   ) => {
-    const yak = (props: TProps, ref: unknown) => {
-      const propsWithTheme = { ...props, theme: useTheme() };
-      const runtimeStyles = css(styles, ...values)(propsWithTheme as any);
+    const getRuntimeStyles = css(styles, ...values);
+
+    const Yak = (props: TProps, ref: unknown) => {
+      const runtimeStyles = getRuntimeStyles(
+        // if the css component does not require arguments
+        // it can be call without arguments and skip calling useTheme()
+        //
+        // this is NOT against the rule of hooks as
+        // getRuntimeStyles is a constant defined outside of the component
+        //
+        // for example
+        //
+        // const Button = styled.button`color: red;`
+        //       ^ does not need to have access to theme
+        //
+        // const Button = styled.button`${({ theme }) => css`color: ${theme.color};`}`
+        //       ^ must be have acces to theme
+        (getRuntimeStyles.length
+          ? { ...props, theme: useTheme() }
+          : {}) as TProps
+      );
+      // props includes all values for the css and for the html
+      // however if Component is a string e.g. "div" only the html props should be passed
+      // 
+      // similar to styled-components 6.0.0 all props that start with a $ sign are removed
       const filteredProps =
         typeof Component === "string" ? removePrefixedProperties(props) : props;
+      // merge the users props with the generated runtime className and style
       const mergedProps = {
         ...filteredProps,
-        style: { ...(props.style || {}), ...runtimeStyles.style },
+        style: { ...props.style as {}, ...runtimeStyles.style },
         className:
           (props.className ? props.className + " " : "") +
           runtimeStyles.className,
-      }
+      };
       // if the styled(Component) syntax is used and the component is a yak component
       // we can call the yak function directly to avoid an unnecessary wrapper with an additional
       // forwardRef call
-      if (typeof Component !== "string" && "yak" in (Component as (FunctionComponent<any> & {yak?: FunctionComponent<any>}))) {
-        return (Component as (FunctionComponent<any> & {yak: FunctionComponent<any>})).yak(mergedProps, ref);
+      if (
+        typeof Component !== "string" &&
+        "Yak" in
+          (Component as FunctionComponent<any> & {
+            Yak?: FunctionComponent<any>;
+          })
+      ) {
+        return (
+          Component as FunctionComponent<any> & { Yak: FunctionComponent<any> }
+        ).Yak(mergedProps, ref);
       }
-      return (
-        <Component
-          ref={ref as any}
-          {...mergedProps}
-        />
-      );
+      return <Component ref={ref as any} {...mergedProps} />;
     };
-    return Object.assign(React.forwardRef(yak), {yak});
+    return Object.assign(React.forwardRef(Yak), { Yak });
   };
-};
+}
 
 /**
- * The `styled` method works perfectly on all of your own or any third-party component, 
+ * The `styled` method works perfectly on all of your own or any third-party component,
  * as long as they attach the passed className prop to a DOM element.
- * 
+ *
  * @usage
- * 
+ *
  * ```tsx
  * const StyledLink = styled(Link)`
  *  color: #BF4F74;
@@ -76,12 +103,12 @@ export const styled = new Proxy(StyledFactory, {
     }
     return target(TagName as keyof JSX.IntrinsicElements);
   },
-}) as (
-  <TBaseProps extends {}>(Component: FunctionComponent<TBaseProps>) => <TProps extends {}>(
-    styles: TemplateStringsArray,
-    ...values: CSSInterpolation<TProps & { theme: YakTheme }>[]
-  ) => FunctionComponent<TBaseProps & TProps>
-) & {
+}) as (<TBaseProps extends {}>(
+  Component: FunctionComponent<TBaseProps>
+) => <TProps extends {}>(
+  styles: TemplateStringsArray,
+  ...values: CSSInterpolation<TProps & { theme: YakTheme }>[]
+) => FunctionComponent<TBaseProps & TProps>) & {
   [TagName in HtmlTags]: ReturnType<typeof StyledFactory<TagName>>;
 };
 
