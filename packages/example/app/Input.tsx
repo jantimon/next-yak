eff9499b40a64c06630e6df309afe623401df24a
Jan Nicklas <j.nicklas@me.com>
2023-11-20 15:13:14 +0100

    fix css

--- packages/example/app/Input.tsx
+++ packages/example/app/Input.tsx
@@ -27,7 +27,7 @@ const Wrapper = styled.div`
   transition: background-color 0.5s ease-in-out;
   &:has(
       ${Input}:where(:hover, :focus),
-      ${PasswordInput}::where(:hover, :focus)
+      ${PasswordInput}:where(:hover, :focus)
     ) {
     background-color: #4c4c4cb9;
   }
