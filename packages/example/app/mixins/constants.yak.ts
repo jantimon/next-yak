6a6a16285bea1708cc17b727d5b51be9e5f37462
Matthias Jäggli <matthias.jaeggli@gmail.com>
2024-01-24 16:19:31 +0100

    Add example

---
+++ packages/example/app/mixins/constants.yak.ts
@@ -0,0 +1,13 @@
+import { css } from "next-yak";
+
+export const typography = {
+  h1: css`
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+    ${css`
+      letter-spacing: 0.00938em;
+    `}
+  `,
+};
