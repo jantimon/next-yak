3ec51908670683cd50d61eae7f5d087a9802f08f
Remo Vetere <remo.vetere@gmail.com>
2024-02-14 16:54:22 +0100

    Rebuild internals

--- packages/next-yak/dist/runtime-internals/index.js
+++ packages/next-yak/dist/runtime-internals/index.js
@@ -1,2 +1,2 @@
-var o=(t,i)=>(...n)=>t(...n)+i;export{o as __yak_unitPostFix};
+var n=(t,o)=>typeof t=="function"?(...i)=>t(...i)+o:t+o;export{n as __yak_unitPostFix};
 //# sourceMappingURL=index.js.map
