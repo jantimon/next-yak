c3ad114757ee6068634a51d2e9e471ce9e915ec5
Remo Vetere <remo.vetere@gmail.com>
2024-02-14 10:59:33 +0100

    Cleanup

--- packages/next-yak/loaders/lib/extractCssUnit.cjs
+++ packages/next-yak/loaders/lib/extractCssUnit.cjs
@@ -1,19 +1,6 @@
 /**
  * Extracts the css unit from a css string and checks if it is a valid CSS unit
  *
- * example:
- * " "                                                      => null
- * ";\n  display: block;\n  "                               => null
- * " ease-out 1s infinite reverse both paused slidein;\n"   => null
- * ";\n    "                                                => null
- * "rem;\n        margin: "                                 => rem
- * "px;\n        z-index: "                                 => px
- * ";\n        transform: translateX("                      => null
- * "px) translateY("                                        => px
- * "px);\n        font-family: "Arial", sans-serif;\n     " => px
- * "deg)\n          translate(0, -88px) rotate("            => deg
- * "deg);\n     "                                           => deg
- *
  * @param {string} quasi
  */
 function extractCssUnit(quasi) {
