6a86fdb063326a44d1ad861ae78122932d9ce4c9
Jan Nicklas <j.nicklas@me.com>
2024-05-14 09:21:53 +0200

    Benchmark rerender inp logger (#95)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/benchmark/app/styled/layout.tsx
+++ packages/benchmark/app/styled/layout.tsx
@@ -5,5 +5,22 @@ export default function StyledLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+    *, *::before, *::after { box-sizing: border-box; }
+    .box:nth-last-child(-n+1) { display: block; }
+    :not(:last-child):after { box-sizing: border-box; }
+    :not(:last-child):before { box-sizing: border-box; }
+    :not(:last-child) + :before { box-sizing: border-box; }
+    :not(:last-child) + :after { box-sizing: border-box; }
+    :not([foo]):not([bar]) { box-sizing: border-box; }
+    `,
+        }}
+      />
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </>
+  );
 }
