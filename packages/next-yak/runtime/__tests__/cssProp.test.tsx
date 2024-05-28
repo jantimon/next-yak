f5b92a8b3b803adf4abc7b1b1d3a7b43a74b4c4d
Luca Schneider <luca.schneider@gmx.ch>
2024-05-28 21:55:33 +0200

    Added runtime tests for css prop merging function

---
+++ packages/next-yak/runtime/__tests__/cssProp.test.tsx
@@ -0,0 +1,18 @@
+import { it, expect } from "vitest";
+import {mergeCssProp} from "../internals/mergeCssProp"
+
+it("merge properties when className is set", async () => {
+			expect(mergeCssProp({className: "foo"}, {className: "cssProp", style: {"--any-var": "any"}})).toMatchObject({className: "foo cssProp", style: {"--any-var": "any"}});
+    });
+    it("merge properties when style is set", async () => {
+			expect(mergeCssProp({style: {padding: "5px"}}, {className: "cssProp", style: {"--any-var": "any"}})).toMatchObject({className: "cssProp", style: {padding: "5px", "--any-var": "any"}});
+    });
+    it("merge properties when spreaded property is set", async () => {
+			expect(mergeCssProp({className: "foo"}, {className: "cssProp", style: {"--any-var": "any"}})).toMatchObject({className: "foo cssProp", style: {"--any-var": "any"}});
+    });
+    it("merge properties when class name and style is set", async () => {
+			expect(mergeCssProp({className: "foo", style: {padding: "5px"}}, {className: "cssProp", style: {"--any-var": "any"}})).toMatchObject({className: "foo cssProp", style: {padding: "5px", "--any-var": "any"}});
+    });
+    it("merge properties when class name, style and spreaded property is set", async () => {
+			expect(mergeCssProp({className: "foo", style: {padding: "5px"}}, {className: "cssProp", style: {"--any-var": "any"}})).toMatchObject({className: "foo cssProp", style: {padding: "5px", "--any-var": "any"}});
+    });
