292137d5f7142c5aa41b703a7a05b0478d6e5a1b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-05 20:42:14 +0900

    Update test refs

--- packages/yak-swc/yak_swc/tests/fixture/cross-file/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/cross-file/output.tsx
@@ -3,6 +3,6 @@ import { styled, css, keyframes } from "next-yak";
 // @ts-ignore
 import { Icon } from "./Icon";
 const primary = "green";
-const Button = styled.button`Button`;
-const Animation = keyframes`Animation`;
-const Wrapper = styled.div`Wrapper`;
+const Button = styled.button/*EXTRACTED*/ `Button`;
+const Animation = keyframes/*EXTRACTED*/ `Animation`;
+const Wrapper = styled.div/*EXTRACTED*/ `Wrapper`;
