157785cfa867a272ea1e6be77c024a55c09ce492
Jan Nicklas <j.nicklas@me.com>
2024-07-14 19:11:56 +0200

    adjust test

--- packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/input.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/input.tsx
@@ -2,9 +2,9 @@ import { styled } from "next-yak";
 
 export const FlexContainer = styled.div`
   display: flex;
+  align-items: ${({ $align }) => $align || 'stretch'};
   flex-direction: ${({ $direction }) => $direction || 'row'};
   justify-content: ${({ $justify }) => $justify || 'flex-start'};
-  align-items: ${({ $align }) => $align || 'stretch'};
   padding: 20px;
   background-color: #f0f0f0;
 `;
