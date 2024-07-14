99ebb21edf82c825781a862a75e46d0b092e847c
Jan Nicklas <j.nicklas@me.com>
2024-07-14 19:12:22 +0200

    adjust test

--- packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/output.tsx
@@ -3,9 +3,9 @@ import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css"
 export const FlexContainer = /*YAK Extracted CSS:
 .FlexContainer {
   display: flex;
+  align-items: var(--FlexContainer__align-items_o1wkyu);
   flex-direction: var(--FlexContainer__flex-direction_o1wkyu);
   justify-content: var(--FlexContainer__justify-content_o1wkyu);
-  align-items: var(--FlexContainer__align-items_o1wkyu);
   padding: 20px;
   background-color: #f0f0f0;
 }
