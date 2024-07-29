c252cfc2a7245b9caa4826f7287449512d92ce3c
Jan Nicklas <j.nicklas@me.com>
2024-07-29 10:53:24 +0200

    remove unused function

--- packages/yak-swc/css_in_js_parser/src/to_css.rs
+++ packages/yak-swc/css_in_js_parser/src/to_css.rs
@@ -1,5 +1,5 @@
 //! Converts a list of CSS declarations to a CSS string
-use crate::{parse_css, CssScope, Declaration, ParserState};
+use crate::{CssScope, Declaration};
 
 pub fn to_css(declarations: &[Declaration]) -> String {
   let mut css = String::new();
@@ -44,33 +44,6 @@ pub fn to_css(declarations: &[Declaration]) -> String {
   }
 
   css
-}
-
-// TODO: remove
-pub fn to_css_with_state(
-  declarations: &[Declaration],
-  parser_state: Option<ParserState>,
-) -> String {
-  // In CSS it is allowed to leave the last semicolon out
-  // However the parser can't handle this as a simicolon or a closing brace is required
-  // to determine the end of a declaration
-  //
-  // The reason behind that is the following example where it is unclear from the syntax wether
-  // the ending is a property declaration or a selector:
-  // .foo { section:has(bar)
-  // .foo { background:url(bar)
-  if let Some(parser_state) = parser_state {
-    if !parser_state.pending_css_segment.is_empty() {
-      let (_, final_declarations) = parse_css(";", Some(parser_state));
-      let combined_declarations: Vec<_> = final_declarations
-        .into_iter()
-        .chain(declarations.iter().cloned())
-        .collect();
-
-      return to_css(&combined_declarations);
-    }
-  }
-  to_css(declarations)
 }
 
 #[cfg(test)]
@@ -173,32 +146,6 @@ mod tests {
       r#"
 .foo {
   color: orange;
-}"#
-    );
-  }
-
-  #[test]
-  fn test_parse_css_merge_without_final_semicolon() {
-    let (state1, declarations1) = parse_css(
-      r#"
-      .foo
-    "#,
-      None,
-    );
-    let (state2, declarations2) = parse_css(
-      r#"
-        {
-          color: orange
-    "#,
-      Some(state1),
-    );
-    let combined_declarations: Vec<_> = declarations1.into_iter().chain(declarations2).collect();
-    assert_eq!(
-      to_css_with_state(&combined_declarations, Some(state2)),
-      r#"
-.foo {
-  color: orange
-;
 }"#
     );
   }
