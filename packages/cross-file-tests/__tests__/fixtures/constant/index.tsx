db8804e9e5af93710450cd1796b7645dabe03056
Jan Nicklas <j.nicklas@me.com>
2024-08-16 11:22:09 +0200

    cross-file-tests (#155)

---
+++ packages/cross-file-tests/__tests__/fixtures/constant/index.tsx
@@ -0,0 +1,6 @@
+import { styled } from "next-yak";
+import { siteMaxWidth } from "./constants";
+export const Button = styled.button`
+  color: red;
+  height: ${siteMaxWidth}px;
+`;
---
+++ packages/cross-file-tests/__tests__/fixtures/constant/output/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from "next-yak/internal";
+import __styleYak from "./index.yak.module.css!=!./index?./index.yak.module.css";
+import { siteMaxWidth } from "./constants";
+export var Button = /*YAK Extracted CSS:
+.Button {
+  color: red;
+  height: --yak-css-import: url("./constants:siteMaxWidth")px;
+}
+*/ /*#__PURE__*/ styled.button(__styleYak.Button);
