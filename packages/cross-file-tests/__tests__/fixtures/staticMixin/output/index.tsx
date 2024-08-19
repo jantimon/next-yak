2dc703be61e96d060ec5979eb31a4be184195ac3
Jan Nicklas <j.nicklas@me.com>
2024-08-19 10:22:28 +0200

    add static mixin testcase

---
+++ packages/cross-file-tests/__tests__/fixtures/staticMixin/output/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from "next-yak/internal";
+import __styleYak from "./index.yak.module.css!=!./index?./index.yak.module.css";
+import { lastChildMixin } from './mixin';
+var ListItem = /*YAK Extracted CSS:
+.ListItem {
+  margin-bottom: 10px;
+  &:hover {
+    --yak-css-import: url("./mixin:lastChildMixin");
+  }
+}
+*/ /*#__PURE__*/ styled.li(__styleYak.ListItem, lastChildMixin);
+export default ListItem;
---
+++ packages/cross-file-tests/__tests__/fixtures/staticMixin/output/mixin.tsx
@@ -0,0 +1,6 @@
+import { css } from "next-yak/internal";
+export var lastChildMixin = /*YAK EXPORTED MIXIN:lastChildMixin
+&:last-child {
+  margin-bottom: 0;
+}
+*/ /*#__PURE__*/ css();
