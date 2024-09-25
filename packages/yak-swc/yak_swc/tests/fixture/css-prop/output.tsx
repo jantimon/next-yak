573ebde5de7687944a551bdd08b32d2724d124b1
Luca Schneider <luca.schneider@digitecgalaxus.ch>
2024-09-25 13:51:17 +0200

    Simple css prop works

--- packages/yak-swc/yak_swc/tests/fixture/css-prop/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/css-prop/output.tsx
@@ -1,4 +1,9 @@
 import { css } from "next-yak/internal";
+import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
 const Elem = ()=><div style={{
         padding: "5px"
-    }} css={/*#__PURE__*/ css()}/>;
+    }} {.../*YAK Extracted CSS:
+.Elem {
+  padding: 10px;
+}
+*/ /*#__PURE__*/ css(__styleYak.Elem, {})}/>;
