540b7731329f3154e68415450f92d607a915ef4c
Jan Nicklas <j.nicklas@me.com>
2024-10-21 16:58:21 +0200

    add support for % css-variables

--- packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/props-and-dynamic-styles/output.tsx
@@ -1,4 +1,4 @@
-import { styled } from "next-yak/internal";
+import { styled, __yak_unitPostFix } from "next-yak/internal";
 import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
 export const FlexContainer = /*YAK Extracted CSS:
 .FlexContainer {
@@ -7,12 +7,16 @@ export const FlexContainer = /*YAK Extracted CSS:
   flex-direction: var(--FlexContainer__flex-direction_o1wkyu);
   justify-content: var(--FlexContainer__justify-content_o1wkyu);
   padding: 20px;
+  margin-bottom: var(--FlexContainer__margin-bottom_o1wkyu);
+  top: var(--FlexContainer__top_o1wkyu);
   background-color: #f0f0f0;
 }
 */ /*#__PURE__*/ styled.div(__styleYak.FlexContainer, {
     "style": {
         "--FlexContainer__align-items_o1wkyu": ({ $align })=>$align || 'stretch',
         "--FlexContainer__flex-direction_o1wkyu": ({ $direction })=>$direction || 'row',
-        "--FlexContainer__justify-content_o1wkyu": ({ $justify })=>$justify || 'flex-start'
+        "--FlexContainer__justify-content_o1wkyu": ({ $justify })=>$justify || 'flex-start',
+        "--FlexContainer__margin-bottom_o1wkyu": __yak_unitPostFix(({ $marginBottom })=>$marginBottom || '0', "px"),
+        "--FlexContainer__top_o1wkyu": __yak_unitPostFix(({ $top })=>$top * 20, "%")
     }
 });
