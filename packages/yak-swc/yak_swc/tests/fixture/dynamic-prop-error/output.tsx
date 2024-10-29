96465bfc1fdb595a7d9a3b4d5e5051885e086a3e
Jan Nicklas <j.nicklas@me.com>
2024-10-29 14:27:15 +0100

    enforce arrow functions

---
+++ packages/yak-swc/yak_swc/tests/fixture/dynamic-prop-error/output.tsx
@@ -0,0 +1,14 @@
+import { styled, __yak_unitPostFix } from "next-yak/internal";
+import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
+export const FlexContainer = /*YAK Extracted CSS:
+.FlexContainer {
+  display: flex;
+  z-index: var(--FlexContainer__z-index_o1wkyu);
+  margin-bottom: var(--FlexContainer__margin-bottom_o1wkyu);
+}
+*/ /*#__PURE__*/ styled.div(__styleYak.FlexContainer, {
+    "style": {
+        "--FlexContainer__margin-bottom_o1wkyu": __yak_unitPostFix(spacing[40].toString(), "px"),
+        "--FlexContainer__z-index_o1wkyu": getZIndex()
+    }
+});
