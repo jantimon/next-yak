1be379eb4aebe9767334141ecceb2e8b127d099f
Jan Nicklas <j.nicklas@me.com>
2024-11-06 11:18:37 +0100

    using matches! macro instead of if let

--- packages/yak-swc/yak_swc/src/lib.rs
+++ packages/yak-swc/yak_swc/src/lib.rs
@@ -504,10 +504,13 @@ where
         }
       }
 
-      // insert it as last import to avoid changing css order issues
+      // search for the last import statement as position to insert the css module import
+      // it has to be the last import to ensure that the css module is loaded after the other imports
+      // and therefore the css is added to the end of the bundle css file
+      // see https://github.com/jantimon/next-yak/issues/202
       let mut last_import_index = 0;
       for (i, item) in module.body.iter_mut().enumerate() {
-        if let ModuleItem::ModuleDecl(ModuleDecl::Import(_import_declaration)) = item {
+        if matches!(item, ModuleItem::ModuleDecl(ModuleDecl::Import(_))) {
           last_import_index = i + 1;
         }
       }
