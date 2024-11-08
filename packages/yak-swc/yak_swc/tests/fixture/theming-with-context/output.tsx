7a080ab923e010e55e2145ebe2a60c1103bdc704
Jan Nicklas <j.nicklas@me.com>
2024-11-08 13:45:27 +0100

    Switch to FNV-1a hash for CSS identifiers (#205)

--- packages/yak-swc/yak_swc/tests/fixture/theming-with-context/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/theming-with-context/output.tsx
@@ -3,14 +3,14 @@ import { styled } from "next-yak/internal";
 import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
 const ThemedComponent = /*YAK Extracted CSS:
 .ThemedComponent {
-  background-color: var(--ThemedComponent__background-color_pMS1gk);
-  color: var(--ThemedComponent__color_pMS1gk);
+  background-color: var(--ThemedComponent__background-color_m7uBBu);
+  color: var(--ThemedComponent__color_m7uBBu);
   padding: 20px;
   border-radius: 8px;
 }
 */ /*#__PURE__*/ styled.div(__styleYak.ThemedComponent, {
     "style": {
-        "--ThemedComponent__background-color_pMS1gk": (props)=>props.theme.background,
-        "--ThemedComponent__color_pMS1gk": (props)=>props.theme.text
+        "--ThemedComponent__background-color_m7uBBu": (props)=>props.theme.background,
+        "--ThemedComponent__color_m7uBBu": (props)=>props.theme.text
     }
 });
