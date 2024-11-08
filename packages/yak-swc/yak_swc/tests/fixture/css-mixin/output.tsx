91c96ac7c193b9c7c3777ed13b238aa3912ba661
Jan Nicklas <j.nicklas@me.com>
2024-11-08 13:24:15 +0100

    switch to fnv1a_64

--- packages/yak-swc/yak_swc/tests/fixture/css-mixin/output.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/css-mixin/output.tsx
@@ -2,13 +2,13 @@ import { styled, css, __yak_unitPostFix } from "next-yak/internal";
 import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
 const buttonStyles = /*#__PURE__*/ css(({ $active })=>$active && /*#__PURE__*/ css(__styleYak.buttonStyles__$active, {
         "style": {
-            "--buttonStyles__max-width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
+            "--buttonStyles__max-width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
         }
     }), function({ $letters }) {
     return $letters > 5 && /*#__PURE__*/ css(__styleYak.buttonStyles__);
 }, {
     "style": {
-        "--buttonStyles__width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
+        "--buttonStyles__width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
     }
 });
 export const ThemedButton = /*YAK Extracted CSS:
@@ -20,23 +20,23 @@ export const ThemedButton = /*YAK Extracted CSS:
 }
 .ThemedButton__$active {
   background-color: #f0f0f0;
-  max-width: var(--ThemedButton__max-width_pMS1gk);
+  max-width: var(--ThemedButton__max-width_m7uBBu);
 }
 .ThemedButton {
-  width: var(--ThemedButton__width_pMS1gk);
+  width: var(--ThemedButton__width_m7uBBu);
 }
 .ThemedButton__ {
   color: red;
 }
 */ /*#__PURE__*/ styled.button(__styleYak.ThemedButton, ({ $active })=>$active && /*#__PURE__*/ css(__styleYak.ThemedButton__$active, {
         "style": {
-            "--ThemedButton__max-width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
+            "--ThemedButton__max-width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
         }
     }), function({ $letters }) {
     return $letters > 5 && /*#__PURE__*/ css(__styleYak.ThemedButton__);
 }, {
     "style": {
-        "--ThemedButton__width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
+        "--ThemedButton__width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
     }
 });
 export const CustomThemedButton = /*YAK Extracted CSS:
@@ -49,22 +49,22 @@ export const CustomThemedButton = /*YAK Extracted CSS:
 }
 .CustomThemedButton__$active {
   background-color: #f0f0f0;
-  max-width: var(--CustomThemedButton__max-width_pMS1gk);
+  max-width: var(--CustomThemedButton__max-width_m7uBBu);
 }
 .CustomThemedButton {
-  width: var(--CustomThemedButton__width_pMS1gk);
+  width: var(--CustomThemedButton__width_m7uBBu);
 }
 .CustomThemedButton__ {
   color: red;
 }
 */ /*#__PURE__*/ styled.button(__styleYak.CustomThemedButton, ({ $active })=>$active && /*#__PURE__*/ css(__styleYak.CustomThemedButton__$active, {
         "style": {
-            "--CustomThemedButton__max-width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
+            "--CustomThemedButton__max-width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 15, "px")
         }
     }), function({ $letters }) {
     return $letters > 5 && /*#__PURE__*/ css(__styleYak.CustomThemedButton__);
 }, {
     "style": {
-        "--CustomThemedButton__width_pMS1gk": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
+        "--CustomThemedButton__width_m7uBBu": __yak_unitPostFix(({ $letters })=>$letters * 10, "px")
     }
 });
