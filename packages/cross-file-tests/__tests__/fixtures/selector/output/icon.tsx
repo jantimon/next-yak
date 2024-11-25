e6dfbfec36f98fe54730941e29390c79ba80c9d3
Jan Nicklas <j.nicklas@me.com>
2024-11-25 21:56:52 +0900

    update snapshots

--- packages/cross-file-tests/__tests__/fixtures/selector/output/icon.tsx
+++ packages/cross-file-tests/__tests__/fixtures/selector/output/icon.tsx
@@ -1,4 +1,4 @@
 import { styled } from "next-yak/internal";
-import __styleYak from "./icon.yak.module.css!=!./icon?./icon.yak.module.css";
+import "./icon.yak.module.css!=!./icon?./icon.yak.module.css";
 export var Icon = /*#__PURE__*/ styled.svg();
 export var AnyIcon = /*#__PURE__*/ styled.svg();
