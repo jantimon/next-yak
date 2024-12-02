d446d4dbeefda52731f2ea8e9c150b974c0de93b
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2024-12-03 00:58:03 +0900

    add test to handle unknown elements

---
+++ packages/yak-swc/yak_swc/tests/fixture/transform-only-known-elements/input.tsx
@@ -0,0 +1,9 @@
+import { styled } from "next-yak";
+
+export const Button = styled.button`
+  background-color: #007bff;
+`;
+
+export const CustomElement = styled.unknown`
+  margin-bottom: 23px;
+`
