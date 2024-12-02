a95e34c03d72eccf47dfac701c6a00537a441991
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2024-12-02 22:51:21 +0900

    Add test which contains multiple components of the same type

---
+++ packages/yak-swc/yak_swc/tests/fixture/multiple-styled-components/output.tsx
@@ -0,0 +1,22 @@
+import { __yak_button, styled } from "next-yak/internal";
+import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
+export const Button = /*YAK Extracted CSS:
+.Button {
+  background-color: #007bff;
+}
+*/ /*#__PURE__*/ __yak_button(__styleYak.Button);
+export const Button2 = /*YAK Extracted CSS:
+.Button2 {
+  background-color: #11c817;
+}
+*/ /*#__PURE__*/ __yak_button(__styleYak.Button2);
+export const FancyButton = /*YAK Extracted CSS:
+.FancyButton {
+  margin-bottom: 23px;
+}
+*/ /*#__PURE__*/ styled(Button)(__styleYak.FancyButton);
+export const FancyButton2 = /*YAK Extracted CSS:
+.FancyButton2 {
+  margin-bottom: 17px;
+}
+*/ /*#__PURE__*/ styled(Button2)(__styleYak.FancyButton2);
