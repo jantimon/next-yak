7b7ead03e3b3e1dea5aa9d6fa92ffe3a5243f80d
Luca Schneider <luca.schneider@gmx.ch>
2024-12-30 14:46:56 +0100

    Fix css prop handling in exported components

--- packages/yak-swc/yak_swc/tests/fixture/css-prop-in-exported-component/output.dev.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/css-prop-in-exported-component/output.dev.tsx
@@ -1,10 +1,10 @@
 import { css, __yak_mergeCssProp } from "next-yak/internal";
 import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
-export const YakLogo = ()=><div {.../*YAK EXPORTED MIXIN:YakLogo
+export const YakLogo = ()=><div {.../*YAK Extracted CSS:
 .YakLogo {
   display: flex;
   gap: 6px;
 }
-*/ /*#__PURE__*/ css()({})}>
+*/ /*#__PURE__*/ css(__styleYak.YakLogo)({})}>
     Yak
   </div>;
