24aa898fedd35967b355e23ad87aa355b8868299
Luca Schneider <luca.schneider@gmx.ch>
2024-12-30 14:43:03 +0100

    Add failing test

---
+++ packages/yak-swc/yak_swc/tests/fixture/css-prop-in-exported-component/output.prod.tsx
@@ -0,0 +1,10 @@
+import { css, __yak_mergeCssProp } from "next-yak/internal";
+import __styleYak from "./input.yak.module.css!=!./input?./input.yak.module.css";
+export const YakLogo = ()=><div {.../*YAK EXPORTED MIXIN:YakLogo
+.YakLogo {
+  display: flex;
+  gap: 6px;
+}
+*/ /*#__PURE__*/ css()({})}>
+    Yak
+  </div>;
